48801f92d1ab715588f577c5a29a32f701ab7615
AshutoshRaj1260 <ashutoshrajofficial@gmail.com>
2026-04-11 15:53:20 +0530

    fix: resolve case sensitivity issue for UserProfile import

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -3,7 +3,7 @@ import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import Dashboard from "../features/chat/pages/Dashboard";
 import Protected from "../features/auth/components/Protected";
-import UserProfile from "../features/profile/pages/userProfile";
+import UserProfile from "../features/profile/pages/UserProfile";
 
 export const router = createBrowserRouter([
   {
