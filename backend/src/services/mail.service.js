09a575c209a313951517bb8beab9b1c24d3a4739
AshutoshRaj1260 <ashutoshrajofficial@gmail.com>
2026-04-12 13:32:36 +0530

    fix: update nodemailer transport configuration for email service

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -2,6 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     type: "OAuth2",
     user: process.env.GOOGLE_USER,
@@ -9,6 +12,9 @@ const transporter = nodemailer.createTransport({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 transporter
